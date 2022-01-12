7d27990fc5ef9f7dbcb1315647e405569441c654
Tojo Antonio ANDRIAMANGA <a.tojo.antonio@gmail.com>
2022-01-12 13:59:47 +0100

    add storybook & github workflow hidden folder

---
+++ .storybook/preview.js
@@ -0,0 +1,25 @@
+import { GlobalStyle } from "../src/shared/global";
+
+/*
+ * Global decorator to apply the styles to all stories
+ * Read more about them at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
