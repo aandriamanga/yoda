33daf51792443f0cb4b325815769ac52fb0e5ef9
Tojo Antonio ANDRIAMANGA <a.tojo.antonio@gmail.com>
2022-01-12 14:37:49 +0100

    add A11y addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -22,4 +22,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
+  },
 };
